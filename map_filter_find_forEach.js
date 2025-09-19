4a2fac69a2b5e8a6483cb7dbe3fbe2658332e31d
mehedihasan53 <mehedihasan.codes3@gmail.com>
2025-09-19 23:55:36 +0600

    map_filter_find_forEach

---
+++ map_filter_find_forEach.js
@@ -0,0 +1,13 @@
+const products = [
+    { name: 'laptop', price: 1000, count: 5 },
+    { name: 'desktop', price: 1500, count: 3 },
+    { name: 'phone', price: 500, count: 10 },
+    { name: 'tablet', price: 700, count: 8 },
+    { name: 'tv', price: 1200, count: 2 },
+]
+
+// 1. map
+const prices = products.map(p => p.price)
+// console.log(prices)
+// 2.forEach
+products.forEach(p => console.log(p.name))
